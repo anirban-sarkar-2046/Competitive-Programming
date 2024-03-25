dcb649651088862312cffcaa8f242687e7756223
Anirban2046 <56195705+Anirban2046@users.noreply.github.com>
2024-03-25 12:55:18 +0600

    HackerRank - plus-minus.cpp

--- HackerRank Problems Solved/HackerRank - plus-minus.cpp
+++ HackerRank Problems Solved/HackerRank - plus-minus.cpp
@@ -1,12 +1,19 @@
-void plusMinus(vector<int> arr) {
-    int pos=0, mini=0, zero=0, n=arr.size();
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int arr[n];
+    int pos=0, neg=0, zero=0;
     for(int i=0; i<n; i++){
+        cin >> arr[i];
        if(arr[i]==0) zero++;
        else if(arr[i]>0) pos++;
-       else mini++; 
+       else neg++; 
     }
     cout << fixed << setprecision(6);
     cout << (double)pos/n << '\n';
-    cout << (double)mini/n << '\n';
+    cout << (double)neg/n << '\n';
     cout << (double)zero/n << '\n';
 }
