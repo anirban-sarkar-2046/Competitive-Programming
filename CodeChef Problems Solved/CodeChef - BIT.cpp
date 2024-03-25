8022db36bd607a196e85f776ba5c7fb097921d39
Anirban2046 <56195705+Anirban2046@users.noreply.github.com>
2024-03-26 00:15:19 +0600

    CodeChef - BIT.cpp

---
+++ CodeChef Problems Solved/CodeChef - BIT.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll t; cin >> t;
+    while(t--){
+        ll n; cin >> n;
+
+        ll s=0;
+        for(ll i=1; i<=n; i++){
+            ll c=0;
+            for(ll j=0; j<10; j++){
+                if((i>>j)&1) c++;
+            }
+            s += c;
+         }
+
+        cout << s << '\n'; 
+    }
+    return 0;
+}
