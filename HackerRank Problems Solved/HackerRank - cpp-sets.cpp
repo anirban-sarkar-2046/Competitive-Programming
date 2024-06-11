eb6516c7462806299905064b66c8f479c093a79b
Anirban2046 <56195705+Anirban2046@users.noreply.github.com>
2024-06-12 01:59:06 +0600

    STL

---
+++ HackerRank Problems Solved/HackerRank - cpp-sets.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    ll q; cin >> q;
+    set<ll> st;
+    while(q--){
+        ll x, y; cin >> x >> y;
+        if(x==1) st.insert(y);
+        if(x==2) st.erase(st.find(y));
+        if(x==3) cout << (st.find(y)!=st.end() ? "Yes" : "No") << '\n';
+    }
+    return 0;
+}
