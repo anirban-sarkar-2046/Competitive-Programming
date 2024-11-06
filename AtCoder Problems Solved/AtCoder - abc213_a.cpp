17b6a34cfac23b4908de4bd26e863240acf39374
Anirban Sarkar <147172432+anirban-sarkar-2046@users.noreply.github.com>
2024-11-07 01:43:15 +0600

    Create AtCoder - abc213_a.cpp

---
+++ AtCoder Problems Solved/AtCoder - abc213_a.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int x, y; cin >> x >> y;
+    cout << (x ^ y) << '\n';
+    return 0;
+}
